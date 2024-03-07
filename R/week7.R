91da6978622e1a4975fa21fcec42d065fcebf49b
lisette-18 <horne146@umn.edu>
2024-03-07 17:40:50 -0600

    Figure 5; Final Commit

--- R/week7.R
+++ R/week7.R
@@ -45,3 +45,10 @@ week7_tbl %>%
   labs(x = "Gender", y = "Time Elapsed (mins)")) %>%
   ggsave("../figs/fig4.png",.)
 
+
+(ggplot(week7_tbl, aes(x=q5, y = q7, color = condition)) +
+  geom_jitter() +
+  geom_smooth(method = "lm", se = FALSE) + 
+  labs(x = "Score on Q5", y = "Score on Q7", color = "Experimental Condition") + 
+  theme(legend.position = "bottom",legend.background= element_rect(fill = "#E0E0E0"))) %>%
+  ggsave("../figs/fig5.png",.)
